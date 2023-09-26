db85065d6c7c4865361ffc206838874be4a95e15
lami mershed <mykare.lamimershed@gmail.com>
2023-09-26 16:54:42 +0530

    initail commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,6 +2,7 @@ import type { MetaFunction } from "@remix-run/node";
 import Component2 from "~/components/Component2";
 import Component3 from "~/components/Component3";
 import { Component1 } from "~/components/Component1";
+import { useState } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,11 +12,40 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const [content, setContent] = useState([
+    { item: <Component1 />, itemName: "Component1" },
+    { item: <Component2 />, itemName: "Component2" },
+    { item: <Component3 />, itemName: "Component3" },
+  ]);
+  const swapup = (index: number) => {
+    console.log("==== index", content);
+    const newBlock = [...content];
+    if (index > 0) {
+      let temp = newBlock[index];
+      newBlock[index] = newBlock[index - 1];
+      newBlock[index - 1] = temp;
+      setContent(newBlock);
+    }
+  };
+  const swapdown = (index: number) => {
+    const newBlock = [...content];
+    if (index < newBlock.length - 1) {
+      let temp = newBlock[index];
+      newBlock[index] = newBlock[index + 1];
+      newBlock[index + 1] = temp;
+      setContent(newBlock);
+    }
+  };
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <Component1 />
-      <Component2 />
-      <Component3 />
+      {content.map((item: object, index: number) => (
+        <div>
+          <button onClick={() => swapup(index)}>up</button>
+          {item.item}
+          <button onClick={() => swapdown(index)}>down</button>
+        </div>
+      ))}
     </div>
   );
 }
